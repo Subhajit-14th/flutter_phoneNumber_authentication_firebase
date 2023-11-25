580843eede235e143d89a33aac038b1cab9b3288
Subhajit-14th <subhajitdtt700@gmail.com>
2023-11-25 15:37:43 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/phonenumberauthenticationfirebase/appium/phonenumberauthenticationfirebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.phonenumberauthenticationfirebase.appium.phonenumberauthenticationfirebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
